8ecf94ae12b21d527943153e3f8cea99b361a29b
ssatriya <ssatriya@gmail.com>
2023-11-25 00:10:51 +0800

    Fix profile button on mobile

--- src/components/right/about.tsx
+++ src/components/right/about.tsx
@@ -17,16 +17,22 @@ export default function About() {
           by making project for about 6-8 months now.
         </span>
       </p>
-      <div className="mt-12 lg:hidden flex gap-4">
-        <button>
-          <Icons.github className="h-6 w-6 fill-slate-600 hover:fill-slate-400 transition-colors" />
-        </button>
-        <button>
-          <Icons.twitter className="h-6 w-6 fill-slate-600 hover:fill-slate-400 transition-colors" />
-        </button>
-        <button>
-          <Icons.linkedIn className="h-6 w-6 fill-slate-600 hover:fill-slate-400 transition-colors" />
-        </button>
+      <div className="mt-12 lg:hidden flex gap-4 relative">
+        <a href="https://github.com/ssatriya" target="_blank">
+          <button>
+            <Icons.github className="h-6 w-6 fill-slate-600" />
+          </button>
+        </a>
+        <a href="https://twitter.com/ssatriyaaa" target="_blank">
+          <button>
+            <Icons.twitter className="h-6 w-6 fill-slate-600" />
+          </button>
+        </a>
+        <a href="https://www.linkedin.com/in/ssatriya/" target="_blank">
+          <button>
+            <Icons.linkedIn className="h-6 w-6 fill-slate-600" />
+          </button>
+        </a>
       </div>
     </div>
   );
